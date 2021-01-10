3418d3a7491cf533804de68b9839cd8900b74501
yuhaihe <yhh@kyong.net>
2021-01-10 22:34:00 +0800

    test

--- template.js
+++ template.js
@@ -1,11 +1,64 @@
-let reg =   /{{(\w+)}}/ig;
-let str = '今天我想吃{{eat}},然后{{sleep}}';
-const data = {
-  eat: '苹果',
-  sleep: '睡觉'
+class NodeList {
+  constructor(val) {
+    this.value = val;
+    this.next - null;
+  }
 }
-let d = str;
-str.replace(reg, function(s, n1){
-  d = d.replace(s, data[n1])
-})
-console.log(d)
+
+class LinkList {
+  constructor() {
+    this.count = 0;
+    this.head = null;
+    this.equalsFn = function (a, b) {
+      return a === b;
+    }
+  }
+
+  push(val) {
+    const node = new NodeList(val);
+    if (this.head == null) {
+      this.head = node
+    } else {
+      let cur = this.head;
+      while (cur.next != null) {
+        cur = cur.next;
+      }
+      cur.next = node;
+    }
+    this.count++;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        let previous;
+        for (let i = 0; i < index; i++) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.value;
+    } else {
+      return undefined;
+    }
+  }
+  removeElement(element) {
+
+  }
+  getHead() {
+    return this.head;
+  }
+}
+
+let l = new LinkList();
+l.push(1);
+l.push(2);
+l.push(3);
+l.push(4);
+l.removeAt(0)
+console.log(l.getHead())
