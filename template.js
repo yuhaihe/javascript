c0a3d0b04d7f6a12241e5cd291908a82c8b1102d
yuhaihe <yhh@kyong.net>
2021-01-13 23:49:08 +0800

    lianbiao

--- template.js
+++ template.js
@@ -2,130 +2,174 @@
  * push,removeAt,getElementAt,insert,indexOf,remove,isEmpty,size,getHead,toString
  */
 
-class NodeList {
-  constructor (val) {
-    this.value = val;
-    this.next = undefined;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
+class DoubleNode extends Node{
+  constructor(value){
+    super(value);
+    this.prev = null;
+  }
+}
 class LinkList {
-  constructor () {
-    this.count = 0;
+  constructor() {
     this.head = null;
+    this.count = 0;
   }
 
-  push (val) {
-    const node = new NodeList(val);
+  push(val) {
+    const node = new Node(val);
     if (this.head == null) {
-      this.head = node
+      this.head = node;
     } else {
-      let cur = this.head;
-      while (cur.next != null) {
-        cur = cur.next;
+      let current = this.head;
+      while (current.next != null) {
+        current = current.next;
       }
-      cur.next = node;
+      current.next = node;
     }
     this.count++;
+    return this.count;
   }
 
-  removeAt (index) {
+  removeAt(index) {
     if (index >= 0 && index < this.count) {
-      let current = this.head;
+      let head = this.head;
       if (index === 0) {
-        this.head = current.next;
+        this.head = head.next;
       } else {
-        let previous;
-        for (let i = 0; i < index; i++) {
-          previous = current;
-          current = current.next;
-        }
-        previous.next = current.next;
+        // 上一个
+        let prev = this.getElementAt(index - 1);
+        let current = prev.next;
+        prev.next = current.next;
       }
       this.count--;
-      return current.value;
-    } else {
-      return undefined;
-    }
-  }
-  getElementAt (index) {
-    if (index >= 0 && index <= this.count) {
-      let node = this.head;
-      for (let i = 0; i < index; i++) {
-        node = node.next;
-      }
-      return node;
+      return head.value;
     }
 
     return undefined;
   }
-  insert (element, index) {
-    if (index >= 0 && index <= this.count) {
-      let node = new NodeList(element);
+  getElementAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current;
+      if (index === 0) {
+        current = this.head;
+      } else {
+        current = this.head;
+        for (let i = 0; i < index; i++) {
+          current = current.next;
+        }
+      }
+      return current;
+    }
+    return undefined;
+  }
+  insert(element, index) {
+    if (index >= 0 && index < this.count) {
+      let node = new Node(element)
       if (index === 0) {
         node.next = this.head;
         this.head = node;
       } else {
-        let previous = this.getElementAt(--index);
-        let current = previous.next;
-        previous.next = node;
-        node.next = current;
+        // 上一个
+        let prev = this.getElementAt(index - 1);
+        let next = prev.next;
+        node.next = next;
+        prev.next = node;
       }
       this.count++;
-      return true;
+      return element;
     }
 
     return undefined;
   }
-  indexOf (element) {
-    let node = this.head;
+  indexOf(element) {
+    let current = this.head;
     for (let i = 0; i < this.count; i++) {
-      if (node.value === element) {
+      if (current.value === element) {
         return i;
       }
-      node = node.next;
+      current = current.next;
     }
 
     return -1;
   }
-  getHead () {
+  getHead() {
     return this.head;
   }
-  remove (element) {
-    let node = this.head;
-    for (let i = 0; i < this.count; i++) {
-      if (node.value === element) {
-        this.removeAt(i);
-        this.count--;
-        return i;
-      }
-      node = node.next;
-    }
-
-    return false;
+  remove(element) {
+    const index = this.indexOf(element);
+    this.removeAt(index);
   }
-  isEmpty () {
+  isEmpty() {
     return this.size() === 0;
   }
-  size () {
+  size() {
     return this.count;
   }
-  toString () {
-    if (this.isEmpty()) return '';
-    let str = `${this.head.value}`;
-    let current = this.head.next;
-    for (let index = 1; index < this.size(); index++) {
-      str = `${str}, ${current.value}`
+  toString() {
+    let current = this.head;
+    let str = `${current.value}`;
+    for (let i = 1; i < this.count; i++) {
       current = current.next;
+      str = `${str},${current.value}`;
     }
     return str;
   }
 }
 
-let l = new LinkList();
+class DoubleLinkList extends LinkList {
+  constructor() {
+    super();
+  }
+  push(val) {
+    const node = new DoubleNode(val);
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
+      node.prev = current;
+      current.next = node;
+    }
+    this.count++;
+    return this.count;
+  }
+  insert(element, index) {
+    if (index >= 0 && index < this.count) {
+      let node = new DoubleNode(element)
+      let head = this.head;
+      if (index === 0) {
+        head.prev = node;
+        node.next = head;
+        this.head = node;
+      } else {
+        // 上一个
+        let prev = this.getElementAt(index - 1);
+        let next = prev.next;
+        node.next = next;
+        node.prev = prev;
+        prev.next = node;
+      }
+      this.count++;
+      return element;
+    }
+
+    return undefined;
+  }
+}
+let l = new DoubleLinkList();
 l.push(1);
 l.push(2);
 l.push(3);
-l.push(4);
-l.push('five');
-console.log(l.toString());
+l.insert(1.5, 1)
+console.log(l.getHead())
+
+
+
