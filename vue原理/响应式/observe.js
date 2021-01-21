8e84f3ccc75975c132a4d13d19afc11339e2c5ab
yuhaihe <luoage@msn.cn>
2021-01-21 18:16:59 +0800

    ttt

--- vue原理/响应式/observe.js
+++ vue原理/响应式/observe.js
@@ -1,6 +1,18 @@
+
 function updateView () {
-  console.log('视图更新');
+  console.log('视图更新' + JSON.stringify(data));
 }
+
+// 重新定义数组原型
+const oldArrayProperty = Array.prototype;
+// 创建对象，原型指向 oldArrayProperty，可以继续扩展新方法
+const arrProto = Object.create(oldArrayProperty);
+['push', 'pop', 'shift', 'unshift', 'splice'].forEach(methodsName => {
+  arrProto[methodsName] = function() {
+    oldArrayProperty[methodsName].call(this, ...arguments);
+    updateView(); // 触发视图更新
+  };
+})
 
 function defineReactive (target, key, value) {
   // 深度监听
@@ -24,6 +36,10 @@ function observe (target) {
     return target;
   }
 
+  if(Array.isArray(target)) {
+    target.__proto__ = arrProto;
+  }
+
   // 从新定义各个属性 （for-in也可以遍历数组）
   for (const key in target) {
     defineReactive(target, key, target[key]);
@@ -31,17 +47,21 @@ function observe (target) {
 }
 
 // 准备数据
-const data = {
+let data = {
   name: '张三',
   age: 20,
   info: {
     address: '北京'
-  }
+  },
+  nums: [10, 20, 30]
 }
 // 监听数据
 observe(data);
 
 // 测试
-data.name = '李四';
-data.age = 25;
-data.info.address = '上海'
+
+// data.age = 25;
+// data.name = '李四';
+// data.info.address = '上海';
+data.nums.push(40)
+
---
+++ vue原理/虚拟dom/snabbdom.js
@@ -0,0 +1,26 @@
+const snabbdom = window.snabbdom;
+
+const patch = snabbdom.init([ // Init patch function with chosen modules
+  snabbdom_class, // makes it easy to toggle classes
+  snabbdom_props, // for setting properties on DOM elements
+  snabbdom_style, // handles styling on elements with support for animations
+  snabbdom_eventlisteners, // attaches event listeners
+]);
+
+const h = snabbdom.h;
+
+const container = document.getElementById('container');
+const vnode = h('ul#list', {}, [
+  h('li.Item', {}, 'Item1'),
+  h('li.Item', {}, 'Item2'),
+])
+patch(container, vnode)
+
+document.getElementById('btn-change').onclick = function() {
+  const newVnode = h('ul#list', {}, [
+    h('li.Item', {}, 'Item1'),
+    h('li.Item', {}, 'ItemB'),
+    h('li.Item', {}, 'Item3'),
+  ]);
+  patch(vnode, newVnode);
+}
