3853d1ef8573137144f641e7a18ad3124a9dea4d
yuhaihe <121786502@qq.com>
2021-02-03 01:04:34 +0800

    feat:react

---
+++ react-demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaseUse from './components/baseUse/index'
+
+function App() {
+  return (
+    <div>
+     <BaseUse/>
+    </div>
+  );
+}
+
+export default App;
